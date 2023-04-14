e3cebf3bcabca0659be234f8b7aac8f04db64a74
Josh McKinney <joshka@users.noreply.github.com>
2023-04-14 16:31:07 -0700

    ci: add lib.rs to make test --doc not fail in ci

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! A small rust package to exercise the Mastodon API using the
+//! [mastdon-async](https://crates.io/crates/mastodon-async) crate.
+//!
+//! # Usage
+//!
+//! ```bash
+//! $ cargo run -- --help
+//! ```
+//!
+//! # License
+//!
+//! This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for more details.
